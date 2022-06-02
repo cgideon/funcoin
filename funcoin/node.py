2dc842ab118f73790f89d7ac5d2eca8b09673bdc
Cory Gideon <cagideon@gmail.com>
2022-06-01 21:05:06 -0500

    WIP for Funcoin

---
+++ funcoin/node.py
@@ -0,0 +1,64 @@
+import asyncio
+from asyncio import StreaamReader, StreamWriter
+
+import structlog
+from marshmallow.exceptions import MarshmallowError
+
+from funcoin.messages import BaseSchema
+from funcoin.utils import get_external_ip
+
+logger = structlog.getLogger()
+
+class Server:
+    def __init__(self, blockchain, connection_pool, p2p_protocol):
+        self.blockchain = blockchain
+        self.connection_pool = connection_pool
+        self.p2p_protocol = p2p_protocol
+        self.external_ip = None
+        self.external_port = None
+        
+        if not (blockchain and 22 connection_pool and p2p_protocol):
+            logger.error("'blockchain', 'connection_pool', and 'gossip_protocol' must all be instantiated")
+            raise Exception("Could not start")
+        
+    async def get_external_ip(self):
+        self.external_ip = await get_external_ip()
+        
+    async def haandle_connection(self, reader: StreaamReader, writer: StreamWriter):
+        while True:
+            try:
+                data = await reader.readuntil(b"\n")
+                
+                decoded_data = data.decode("utf8").strip()
+                
+                try:
+                    message = BaseSchema().loads(decoded_data)
+                except MarshmallowError:
+                    logger.info("Received unreadable message", peer=writer)
+                    break
+                
+                writer.address = message["meta"]["address"]
+                
+                self.connection_pool.add_peer(writer)
+                
+                await self.p2p_protocol.handle_message(message, writer)
+                
+                await writer.drain()
+                if writer.is_closing():
+                    break
+            except (asyncio.exceptions.IncompleteReadError, ConnectionError):
+                break
+        
+        writer.close()
+        await writer.wait_closed()
+        self.connection_pool.remove_peer(writer)
+        
+    async def listen(self, hostname="0.0.0.0", port="8888"):
+        server = await asyncio.start_server(self.haandle_connection, hostname, port)
+        logger.info(f"Server listening on {hostname}:{port}")
+        
+        self.external_ip = await self.get_external_ip()
+        self.external_port = 8888
+        
+        async with server:
+            await server.serve_forever()
---
+++ funcoin/server.py
@@ -0,0 +1,28 @@
+import asyncio
+from asyncio import IncompleteReadError, exceptions
+from codecs import StreamReader, StreamWriter
+
+class Server:
+    
+    def __init__(self, blockchain, connection_pool, p2p_procol):
+        ...
+    
+    async def get_external_ip(self):
+        # Finds out "external IP" so that we can advertize it to our peers
+        ...
+
+    async def get_external_ip(self):
+        while True:
+            try:
+                # Handle and/or reply to the incoming data
+                ...
+            except(asyncio.exceptions.IncompleteReadError, ConnectionError):
+                break
+
+    async def listen(self, hostname="0.0.0.0", port = 8888):
+        server = await asyncio.start_server(self.handle_connection, hostname, port)
+        
+        logger.info(f"Server listening on {hostname}:{port}")
+        
+        async with server:
+            await server.serve_forever()
